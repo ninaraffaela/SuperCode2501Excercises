9518f588f76d79da54393e4b9ca2caf5c1db4353
ninaraffaela <nina.r.ziegler@gmail.com>
2025-02-26 09:24:00 +0100

    start lesson on objects

---
+++ Modul2/17_IntroVite_andFollowingDays/src/07_Objects/objects.ts
@@ -0,0 +1,29 @@
+// ! Objects
+console.log("%c 👾 Heute Objecte", "background-color: teal");
+
+const movieTitle = "Dune";
+const movieYear = 2021;
+const movieDirector = "Denis Villeneuve";
+const movieDescription = "Leute machen Sachen im Sand";
+const movieActors = ["Timothee Chamalet", "Rebecca Ferguson"];
+const movieRating = 4.8;
+// these variables do not have a connection (other than, they belong to the movie)
+
+// * an Object is a datestructure to join/connect different datetypes.
+
+const duneMovie = {
+    title: "Dune",
+    year: 2021,
+    director: "Denis Villeneuve",
+    description: "Leute machen Sachen im Sand",
+    actors: ["Timothee Chalamet", "Rebecca Ferguson"],
+    rating: 4.8
+};
+
+const jurassicParkMovie = {
+    title: "Jurassic Park",
+    year: 1993,
+    director: "Steven Spielberg",
+    description: "Rooooaaaaaaarrrr",
+    actors: ["Jeff Goldblum"]
+}
